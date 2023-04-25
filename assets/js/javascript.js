5bd7cc27dfbab83bed93f765d03cadc070b5e7a6
GersonADS <shiranuime@gmail.com>
2023-04-25 18:29:13 -0300

    mobile 1

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,3 +1,29 @@
 const $Share_click = document.querySelector('#Share_click');
 
+const $Display_Text = document.querySelector('#display-text');
+
+const $Image_set = document.querySelector('#image-seta');
+
+let checkClick = false;
+
+$Share_click.addEventListener('click',()=>{
+
+    
+    if(!checkClick){
+    $Image_set.style.filter = 'brightness(1000%)';
+
+    $Share_click.style.backgroundColor = 'var(--VD-grayish-blue)'
+
+    $Display_Text.style.display = 'flex';
+    }else {
+        $Image_set.style.filter = 'brightness(100%)';
+
+        $Share_click.style.backgroundColor = ''
+    
+        $Display_Text.style.display = 'none';
+    }
+
+    checkClick === true ? checkClick = false : checkClick =true;
+
+});
 
